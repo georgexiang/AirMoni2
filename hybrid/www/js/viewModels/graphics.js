0c165ed2e50d9ee5014c0d24613cbc81f43fe7f0
georgexiang <xiangzhihua@gmail.com>
2016-10-19 15:46:20 +0800

    Air Monitor JET Part
    Air Monitor JET Part

---
+++ hybrid/www/js/viewModels/graphics.js
@@ -0,0 +1,34 @@
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojchart'],
+function(oj, ko, $)
+{   
+    function ChartModel() {
+        var self = this;
+        
+        /* toggle button variables */
+        self.orientationValue = ko.observable('vertical');      
+        self.lineTypeValue = ko.observable('curved');
+        
+        
+  
+        
+        
+        
+        //console.log("pm25Series:"+localStorage.getItem("pm25Series"));
+        
+        /* chart data */
+       //var lineSeries = [{name : "PM2.5", items : [74, 62, 70, 76, 66,80]} ];
+        var lineSeries = [{name : "PM2.5"} ];
+        lineSeries[0].items=JSON.parse(localStorage.getItem("pm25Series"));
+        //var lineGroups = ["9.21", "9.22", "9.23", "9.24", "9.25","9.26"];
+        var lineGroups = JSON.parse(localStorage.getItem("pm25Groups"));
+        console.log(lineSeries[0].items);
+        this.lineSeriesValue = ko.observableArray(lineSeries);
+        this.lineGroupsValue = ko.observableArray(lineGroups);
+        
+       
+    }
+    
+    var chartModel = new ChartModel();
+    return chartModel;
+    
+});	
